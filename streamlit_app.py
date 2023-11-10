72afc70bc37c0cad78b1799bb8db69e37c637207
jarbelaez <jarbelaez@gmail.com>
2023-11-11 07:41:22 +1100

    Update streamlit_app.py
    Add new item and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Bluerberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie ')
-streamlit.text('Hard boiled free-range egg')
+streamlit.text('🥣 Omega 3 Bluerberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie ')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avocado on Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
