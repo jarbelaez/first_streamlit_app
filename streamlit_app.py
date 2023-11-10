c467d1f8374528edaee468135f14b884ebb8ab8a
jarbelaez <jarbelaez@gmail.com>
2023-11-11 07:37:51 +1100

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Bluerberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie ')
+stremlit.text('Hard boiled free-range egg')
