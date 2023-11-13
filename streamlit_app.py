467958a6132e951e8a29119cb995815c47bc00f6
jarbelaez <jarbelaez@gmail.com>
2023-11-14 09:23:56 +1100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
 
 
 # Add second box to select fruit
-def insert_row_snowflake(new_fruit);
+def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
